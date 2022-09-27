fb5f0433e8f5364990a4d0527841941d69234dc2
Max H <maxplaysbest@gmail.com>
2022-09-27 17:04:01 +1300

    Added button and notes

--- Ember/App.cpp
+++ Ember/App.cpp
@@ -5,6 +5,7 @@
 wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
+	// Opens testing scene at the moment
 	MainFrame* mainFrame = new MainFrame("Ember Engine");
 	mainFrame->SetClientSize(800, 400);
 	mainFrame->Center();
--- Ember/MainFrame.cpp
+++ Ember/MainFrame.cpp
@@ -1,6 +1,7 @@
+// Mainframe is for testing at the moment
 #include "MainFrame.h"
 #include <wx/wx.h>
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
-
+	wxButton* button = new wxButton(this, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
 }
