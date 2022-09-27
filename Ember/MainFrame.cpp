3f38192c3541a4bed4bb0f443cab7f0fc2999959
Max H <maxplaysbest@gmail.com>
2022-09-27 20:22:10 +1300

    added wxLib to repo

--- Ember/MainFrame.cpp
+++ Ember/MainFrame.cpp
@@ -5,4 +5,9 @@
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* backroundPanel = new wxPanel(this);
 	wxButton* button = new wxButton(backroundPanel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
+	wxCheckBox* checkBox = new wxCheckBox(backroundPanel, wxID_ANY, "CheckBox", wxPoint(550, 55));
+
+	wxStaticText* staticText = new wxStaticText(backroundPanel, wxID_ANY, "Static Text - Not Editable", wxPoint(120, 150));
+
+	wxTextCtrl textCtrl = new wxTextCtrl
 }
