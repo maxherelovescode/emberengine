6053a2e1a214888ea8b9576d8788fab94ce5c278
Max H <maxplaysbest@gmail.com>
2022-09-28 18:34:48 +1300

    a

--- Ember/MainFrame.cpp
+++ Ember/MainFrame.cpp
@@ -3,12 +3,6 @@
 #include <wx/wx.h>
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
-	wxPanel* backroundPanel = new wxPanel(this);
-	wxButton* button = new wxButton(backroundPanel, wxID_ANY, "Button", wxPoint(150, 50), wxSize(100, 35));
-	wxCheckBox* checkBox = new wxCheckBox(backroundPanel, wxID_ANY, "CheckBox", wxPoint(550, 55));
-
-	wxStaticText* staticText = new wxStaticText(backroundPanel, wxID_ANY, "Static Text - Not Editable", wxPoint(120, 150));
-
-	wxTextCtrl* textCtrl = new wxTextCtrl(backroundPanel, wxID_ANY, "Boo", wxPoint(500, 145), wxSize(200, -1));
+	
 }
 
