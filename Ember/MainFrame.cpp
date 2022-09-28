16777d9a0a73d6534ad2c2c467972e40873d3454
Max H <maxplaysbest@gmail.com>
2022-09-28 19:51:51 +1300

    slider

--- Ember/MainFrame.cpp
+++ Ember/MainFrame.cpp
@@ -11,6 +11,6 @@ MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title) {
 
 	wxTextCtrl* textCtrl = new wxTextCtrl(backroundPanel, wxID_ANY, "Boo", wxPoint(500, 145), wxSize(200, -1));
 
-
+	wxSlider* slider = new wxSlider(backroundPanel, wxID_ANY, 25, 0, 100, wxPoint(100, 250), wxSize(200, -1));
 }
 
